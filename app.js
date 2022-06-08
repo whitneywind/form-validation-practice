e74c5adb3bc2e451522037a6e54814876d0e236f
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-06-08 15:32:36 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const form1 = document.querySelector('#form');
+const formBtn = document.querySelector('#form-btn');
+
+formBtn.addEventListener('click', (e) => {
+    const inputs = document.querySelectorAll('input');
+    for (let input of inputs) {
+        input.addEventListener('invalid', (e) => {
+            if(input.value === '') {
+                input.setCustomValidity(`${input.name} cannot be empty.`);
+            } else {
+                if (input.name === "email") {
+                    input.setCustomValidity('Looks like this is not an email');
+                } else {
+                    input.setCustomValidity('Invalid entry');
+                }
+            }
+          });
+        input.addEventListener('input', () => {
+            input.setCustomValidity('');
+            input.checkValidity();
+        });
+    }
+});
+
+form1.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+});
